7c09ccfddfac3418a71676b8783f91851a574fda
adel307 <adel.307.ahmed@gmail.com>
2025-09-25 15:39:50 +0300

    new updates

---
+++ main_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views  # Import your views
+
+urlpatterns = [
+    # Add your URL patterns here
+    path('', views.home, name='main'),  # Example pattern
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
-# Create your views here.
+def home(request):
+    context = {
+        'current_time': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    return render(request, 'main.html', context)
