6fdb0cc33c553aa520ec2d11b6da09d8265567e6
adel307 <adel.307.ahmed@gmail.com>
2025-09-29 18:22:28 +0300

    new updates

--- my_books/models.py
+++ my_books/models.py
@@ -19,7 +19,7 @@ class Book (models.Model):
     auther_image    =models.ImageField(upload_to='photos2 %Y %m %d',null=True,blank=True)
     price           =models.DecimalField(max_digits=5,decimal_places=2,null=True,blank=True)
     status          =models.CharField(max_length = 100,choices=book_status,null=True,blank=True)
-    pages           =models.IntegerField(dufualt = 0)
+    pages           =models.IntegerField(default = 0)
     active          =models.BooleanField(default=True)
     retal_price_day =models.DecimalField(max_digits=5,decimal_places=2,null=True,blank=True)
     retal_proid     =models.IntegerField(null=True,blank=True)
