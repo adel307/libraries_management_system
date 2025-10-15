28720c36b2a5e046458df78ac98724bd2a3620ef
adel307 <adel.307.ahmed@gmail.com>
2025-10-16 00:20:57 +0300

    new updates

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render , redirect , get_object_or_404
 from django.http import HttpResponse
 from datetime import datetime
 from my_books.models import *
-from .forms import *                                                                                          
+from .forms import *
 
 
 
--- owner/views.py
+++ owner/views.py
@@ -1,8 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect , get_object_or_404
+from django.http import HttpResponse
+from datetime import datetime
+from my_books.models import *
+from main_app.forms import *
 
 # Create your views here.
 
 def owner_func(request):
 
-    return render(request, 'owner/owner.html')    
+    totalPay = 0
+    for book in Book.objects.all() :
+        if book.price :
+            if book.status == 'solid':
+                totalPay += book.price
+    
+    totalRental = 0
+    for book in Book.objects.all() :
+        if book.total_rental :
+            if book.status == 'rental':
+                totalRental += book.total_rental
+    
+    totalsalarys = totalRental + totalPay
+
+    context = {
+        'current_time' : datetime.now().strftime(f"%Y / %m / %d %H:%M:%S"),
+        'books'        : Book.objects.all(),
+        'catigories'   : Catigory.objects.all(),
+        'forms'        : new_book(),
+        'add_category' : new_category(),
+        'books_num'    : Book.objects.filter(active=True).count(),
+        'avl_books'    : Book.objects.filter(status='availble').count(),
+        'rental_books' : Book.objects.filter(status='rental').count(),
+        'solid_books'  : Book.objects.filter(status='solid').count(),
+        'totalsalarys' : totalsalarys,
+        'totalPay'     : totalPay,
+        'totalRental'  : totalRental,   
+    }
+
+    if request.method == 'POST':
+        save_new_book = new_book(request.POST,request.FILES)
+        if save_new_book.is_valid():
+            save_new_book.save()
+            return render(request, 'main_app/index.html', context)
+
+    if request.method == 'POST':
+        save_new_category = new_category(request.POST,request.FILES)
+        if save_new_category.is_valid():
+            save_new_category.save()
+            return render(request, 'main_app/index.html', context)
+
+    return render(request, 'login_owner/login_owner.html',context)    
 
