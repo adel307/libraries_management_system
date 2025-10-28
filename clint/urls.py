8a0e385360335aeefa51de95391ab9129ec2d53e
adel307 <adel.307.ahmed@gmail.com>
2025-10-29 01:30:32 +0300

    new updates

--- clint/urls.py
+++ clint/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('customer_profile', views.customer_profile, name='customer_profile_path'),
-    path('/<int:customer_id>/edit/', views.edit_customer, name='edit_customer'),
+    #path('/<int:customer_id>/edit/', views.edit_customer, name='edit_customer'),
 ]
