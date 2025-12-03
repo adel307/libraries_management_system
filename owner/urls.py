79a043a0a3c38d53c4070478ed24b9f60ac77dad
adel307 <adel.307.ahmed@gmail.com>
2025-12-03 18:08:54 +0200

    new updates

--- owner/urls.py
+++ owner/urls.py
@@ -2,11 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Add your URL patterns here
+    # 1. صفحة تسجيل دخول المالك
     path('-login', views.owner_login_func, name='owner_login_page'),
-    path('update-book-<int:id>', views.update_book, name='update_book_path'),
+    
+    # 2. لوحة تحكم المالك الرئيسية (مسار فريد)
     path('-page', views.owner_func, name='owner_page_path'),
+    
+    # 3. تحديث كتاب
+    path('update-book-<int:id>', views.update_book, name='update_book_path'),
+    
+    # 4. حذف كتاب
     path('delete-book-<int:id>', views.delete, name='delete_book_path'),
-    path('-page', views.owner_func, name='rented_books_path'),
+    
+    # 5. عرض الكتب المستأجرة (مسار واسم فريد)
     path('-rented-books', views.rented_books_func, name='rented_books_path'),
 ]
